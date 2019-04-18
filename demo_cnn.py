9e5173e554be504003289b1154c923526ce24106
Jovan Araiza <jovan.araiza@wsu.edu>
2019-04-18 11:38:01 -0700

    demo working

--- demo_cnn.py
+++ demo_cnn.py
@@ -12,11 +12,34 @@ from tensorflow.python.keras.models import load_model
 import time
 
 MODEL_PATH = './cnn.h5'
-LETTER_MAP = {'A' : 10, 'B' : 11, 'C': 12, 'D': 13, 'E': 14, 'F':15,'G':16, 'H':17, 'I':18, 'i': 19, 'J':20, 'K': 21, 'L': 22, 'l':23, 'M':24, 'N':25, 'O': 26,
-'P': 27, 'Q' : 28, 'R': 29, 'S': 30, 'T': 31, 'U': 32, 'V': 33, 'W': 34, 'X':35, 'Y': 36, 'Z': 37}
-REV_LETTER_MAP = {v:k for k,v in LETTER_MAP.items()}
-def drawPrediction(img, y_pred, y_class):
-    #img = 255 - img
+LETTER_MAP = {'A' : 0, 'B' : 1, 'C': 2,'D': 3,
+            'E': 4, 'F':5,'G':6, 'H':7, 'I':8,
+            'i': 9, 'J':10, 'K': 11, 'L': 12,
+            'l':13, 'M':14, 'N':15, 'O': 16,
+            'P': 17, 'Q' : 18, 'R': 19, 'S': 20,
+            'T': 21,'U': 22, 'V': 23, 'W': 24,
+            'X':25, 'Y': 26, 'Z': 27}
+
+REVERSE_LETTER_MAP = {v:k for k,v in LETTER_MAP.items()}
+
+FONT = cv.FONT_HERSHEY_SIMPLEX
+SCREEN_TITLE_POS = (10, 500)
+BOTTOM_POS = (10,50)
+TEXT_COLOR = GREEN
+TEXT_SIZE = 1
+TEXT_THICK = 2
+
+
+def write_text(text, image, pos):
+    cv.putText(image, text, pos, FONT, TEXT_SIZE,
+               BLACK, TEXT_THICK+5, cv.LINE_AA)
+    cv.putText(image, text, pos, FONT, TEXT_SIZE,
+               TEXT_COLOR, TEXT_THICK, cv.LINE_AA)
+    return image
+
+
+def prediction(img, y_pred, y_class):
+
     pad_color = 0
     img = np.pad(img, ((0,0), (0,1280-720), (0,0)), mode='constant', constant_values=(pad_color))
 
@@ -27,45 +50,8 @@ def drawPrediction(img, y_pred, y_class):
     x, y = 740, 60
     color = (255, 255, 255)
 
-    text = "Neural Network Output:"
-    cv2.putText(img, text=text, org=(x, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
-                    color=color, lineType=line_type)
-
-    text = "Input:"
-    cv2.putText(img, text=text, org=(30, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
-                    color=color, lineType=line_type)
-    y = 130
-    for i, p in enumerate(y_pred):
-        if i == y_class: color = (255, 218, 158)
-        else: color = (100, 100, 100)
-
-        rect_width = 0
-        if p > 0: rect_width = int(p * 3.3)
-
-        rect_start = 230
-        cv2.rectangle(img, (x+rect_start, y-5), (x+rect_start+rect_width, y-20), color, -1)
-
-        text = "%d: %3.2f" % (i, p)
-        cv2.putText(img, text=text, org=(x, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
-                    color=color, lineType=line_type)
-        y += 60
-    return img
-
-def drawPrediction2(img, y_pred, y_class):
-    #img = 255 - img
-    pad_color = 0
-    img = np.pad(img, ((0,0), (0,1280-720), (0,0)), mode='constant', constant_values=(pad_color))
-
-    line_type = cv2.LINE_AA
-    font_face = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1.3
-    thickness = 2
-    x, y = 740, 60
-    color = (255, 255, 255)
-
-    text = "Neural Network Output:"
-    cv2.putText(img, text=text, org=(x, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
-                    color=color, lineType=line_type)
+    text = "Character prediction"
+    write(text,img,SCREEN_TITLE_POS)
 
     text = "Input:"
     cv2.putText(img, text=text, org=(30, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
@@ -75,12 +61,9 @@ def drawPrediction2(img, y_pred, y_class):
     color = (255, 218, 158)
     for i, p in enumerate(y_pred):
         if i == y_class:
-            if i > 9:
-                y_classchr = REV_LETTER_MAP[i]
-                label = "Pred: %c" % (y_classchr)
+            y_classchr = REV_LETTER_MAP[i]
+            label = "Pred: %c" % (y_classchr)
 
-            else:
-                label = "Pred: %d" % (i)
             conf = "Conf: %3.2f" % (p) + '%'
             y+=60
             cv2.putText(img, text=label, org=(x, y), fontScale=font_scale, fontFace=font_face, thickness=thickness,
@@ -111,7 +94,7 @@ while True:
     # Pre-process the image for prediction
     img_proc = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
     #img_proc = cv2.resize(img_proc, (28, 28),interpolation=cv2.INTER_AREA)
-    ret,img_proc = cv2.threshold(img_proc,80,255,cv2.THRESH_BINARY_INV)
+    ret,img_proc = cv2.threshold(img_proc,80,255,cv2.THRESH_BINARY)
     cv2.imshow('thresh',img_proc)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
