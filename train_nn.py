1bdb4c064c73a05791906e0df20732f93aeb5690
Jovan Araiza <jovan.araiza@wsu.edu>
2019-04-23 21:42:59 -0700

    more graphs

--- train_nn.py
+++ train_nn.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage.transform import resize
 
-from tensorflow.python import keras
+import keras
 
 MODEL_PATH = './cnn.h5'
-DATA = './data'
+DATA_PATH = '../data'
 
 LETTER_MAP = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,
               'A' : 10, 'B' : 11, 'C': 12, 'D': 13, 'E': 14, 'F':15,'G':16,
@@ -21,6 +21,14 @@ LETTER_MAP = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,
               'T': 31, 'U': 32, 'V': 33, 'W': 34, 'X':35, 'Y': 36, 'Z': 37}
 REVERSE_LETTER_MAP = {str(v):k for k,v in LETTER_MAP.items()}
 MYLIST = []
+
+DIM = 28 # pixels
+EPOCHS = 14
+
+# Graph Settings
+LINEG_FIGURE_SIZE = (5,5)
+BARG_FIGURE_SIZE = (6,5) 
+
 def plot_classification_report(results):
     x_labels, x_scores = [],[]
     classes = ['%d' % i for i in range(38)]
@@ -30,33 +38,76 @@ def plot_classification_report(results):
         if label in classes:
             x_labels.append(REVERSE_LETTER_MAP[label])
             x_scores.append(scores['precision'])
-    plt.figure(figsize=(10, 7))
+
+    plt.figure(figsize=BARG_FIGURE_SIZE)
     plt.bar(ind, x_scores, align='center', alpha=0.5)
+
     plt.xticks(ind,x_labels)
-    plt.ylabel('Precision')
+    plt.ylabel('precision')
+    plt.xlabel('class')
     plt.title('Classification Report')
     
-    plt.savefig('gbarplot.png')
+    plt.savefig('Barplot.png')
+
+    plt.cla()
+
+
+def plot_training_history(history):
+
+    plt.figure(figsize=LINEG_FIGURE_SIZE)
+
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+
+    plt.title('Model Accuracy History')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    plt.savefig('EpochsVSAccuracy.png')
+    
+    plt.cla()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    
+    plt.title('Model Loss History')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    plt.savefig('EpochsVSLoss.png')
+    
+    plt.cla()
 
 
 def load_NIST(path):
     x,y = [],[]
     for dir_name in os.listdir(path):
         sub = '/'.join([path,dir_name])
+        # counter = 0
         if os.path.isdir(sub):
             for img in os.listdir(sub):
                 p = '/'.join([sub,img])
                 img = imread(p,as_gray=True)
-                img_r = resize(img,(28,28))
+                img_r = resize(img,(DIM,DIM))
                 x.append(img_r.copy())
                 y.append(LETTER_MAP[dir_name])
+                # counter += 1
+                # if counter >= 300:
+                #     break
     x = np.array(x)
     y = np.array(y)
+
     x, y = shuffle(x, y)
-    X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.33)
-    X_train = X_train.reshape((len(X_train),28,28,1))
-    X_test = X_test.reshape((len(X_test),28,28,1))
+
+    X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.20)
+    
+    X_train = X_train.reshape((len(X_train),DIM,DIM,1))
+    X_test = X_test.reshape((len(X_test),DIM,DIM,1))
+    
     return X_train,y_train,X_test,y_test
+
 
 def load_MINST():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
@@ -64,72 +115,73 @@ def load_MINST():
     x_test = x_test / 255.
     x_train = 1 - x_train # flip to black letters with white backgrounds
     x_test = 1 - x_test
-    x_train = x_train.reshape((60000, 28, 28, 1))
-    x_test = x_test.reshape((10000, 28, 28, 1))
+    x_train = x_train.reshape((60000, DIM, DIM, 1))
+    x_test = x_test.reshape((10000, DIM, DIM, 1))
     return x_train,y_train,x_test,y_test
 
 
-# Load OCR Data
-x_train_NIST,y_train_NIST,x_test_NIST,y_test_NIST = load_NIST(DATA)
+if __name__ == '__main__':
+    # Load OCR Data
+    x_train_NIST,y_train_NIST,x_test_NIST,y_test_NIST = load_NIST(DATA_PATH)
 
-# Load MNIST Data
-x_train_MNIST,y_train_MNIST,x_test_MNIST,y_test_MNIST = load_MINST()
+    # Load MNIST Data
+    x_train_MNIST,y_train_MNIST,x_test_MNIST,y_test_MNIST = load_MINST()
 
-# Combine and shuffle data
-x_train_final = np.concatenate((x_train_NIST, x_train_MNIST), axis=0)
-del x_train_NIST # Lower RAM usage
-del x_train_MNIST
-y_train_final = np.concatenate((y_train_NIST, y_train_MNIST), axis=0)
-del y_train_NIST
-del y_train_MNIST
-x_test_final = np.concatenate((x_test_NIST, x_test_MNIST), axis=0)
-del x_test_NIST
-del x_test_MNIST
-y_test_final = np.concatenate((y_test_NIST, y_test_MNIST), axis=0)
-del y_test_NIST
-del y_test_MNIST
-x_train_final, y_train_final = shuffle(x_train_final, y_train_final)
+    # Combine and shuffle data
+    x_train_final = np.concatenate((x_train_NIST, x_train_MNIST), axis=0)
 
-# Convolutional Neural Network Train
-model = keras.Sequential([
-    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-    keras.layers.Flatten(),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(38, activation='softmax')
-])
-model.summary()
+    # Lower RAM usage
+    del x_train_NIST
+    del x_train_MNIST
 
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
-)
+    y_train_final = np.concatenate((y_train_NIST, y_train_MNIST), axis=0)
 
-history = model.fit(x_train_final, y_train_final, validation_split=0.33, epochs=8)
+    del y_train_NIST
+    del y_train_MNIST
 
-y_pred = model.predict_classes(x=x_test_final)
-print("Test Accuracy: ", accuracy_score(y_test_final, y_pred))
-results = classification_report(y_test_final, y_pred, target_names=[
-        '%d' % i for i in range(38)], digits=3,output_dict=True) 
+    x_test_final = np.concatenate((x_test_NIST, x_test_MNIST), axis=0)
 
-print ('Precision:', precision_score(y_test_final, y_pred ,average='micro'))
+    del x_test_NIST
+    del x_test_MNIST
 
-plot_classification_report(results)
-plt.cla()
-# list all data in history
-print(history.history.keys())
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+    y_test_final = np.concatenate((y_test_NIST, y_test_MNIST), axis=0)
 
-plt.savefig('gAccuEpoch.png')
+    del y_test_NIST
+    del y_test_MNIST
 
-model.save(MODEL_PATH)
+    x_train_final, y_train_final = shuffle(x_train_final, y_train_final)
+
+    # Convolutional Neural Network Train
+    model = keras.Sequential([
+        keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(DIM, DIM, 1)),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        keras.layers.Flatten(),
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(38, activation='softmax')
+    ])
+    model.summary()
+
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    history = model.fit(x_train_final, y_train_final, validation_split=0.20, epochs=EPOCHS)
+
+    y_pred = model.predict_classes(x=x_test_final)
+
+    print("Test Accuracy: ", accuracy_score(y_test_final, y_pred))
+
+
+    results = classification_report(y_test_final, y_pred, target_names=[
+            '%d' % i for i in range(38)], digits=3,output_dict=True) 
+
+    plot_classification_report(results)
+
+    plot_training_history(history)
+
+    model.save(MODEL_PATH)
--- train_rest.py
+++ train_rest.py
@@ -13,9 +13,10 @@ from sklearn.naive_bayes import GaussianNB
 
 from skimage.io import imread
 from skimage.transform import resize
+import matplotlib.pyplot as plt
 
 MODEL_PATH = './cnn.h5'
-DATA = './data'
+DATA = '../data'
 LETTER_MAP = {'A' : 0, 'B' : 1, 'C': 2, 'D': 3,
               'E': 4, 'F':5,'G':6, 'H':7, 'I':8,
               'i': 9, 'J':10, 'K': 11, 'L': 12,
@@ -24,23 +25,51 @@ LETTER_MAP = {'A' : 0, 'B' : 1, 'C': 2, 'D': 3,
               'T': 21, 'U': 22, 'V': 23, 'W': 24,
               'X':25, 'Y': 26, 'Z': 27}
 
+DIM = 28 # pixels
+BARG_FIGURE_SIZE = (6,5)
+
+def plot_results(test_results,train_results):
+    
+    ind = np.arange(4) 
+    width = 0.35
+    
+    clf_test_scores, clf_train_scores, clf_names = [], [], []
+
+    for clf,score in train_results:
+        clf_train_scores.append(score)
+
+    for clf,score in test_results:
+        clf_test_scores.append(score)
+        clf_names.append(clf)
+
+    plt.figure(figsize=BARG_FIGURE_SIZE)
+    plt.bar(ind, clf_test_scores,width,label='Test', align='center', alpha=0.5)
+    plt.bar(ind + width, clf_train_scores,width,label='Train', align='center', alpha=0.5)
+    plt.legend(loc='best')
+    plt.xticks(ind + width / 2, clf_names)
+    plt.ylabel('accuracy')
+    plt.xlabel('classifier')
+    plt.title('Classifier Accuracies')
+    
+    plt.savefig('ClassifiersAccuracy.png')
+
+    plt.cla()
+
 def load_NIST(path):
     x,y = [],[]
-
     for dir_name in os.listdir(path):
-
         sub = '/'.join([path,dir_name])
-
+        counter = 0
         if os.path.isdir(sub):
-
             for img in os.listdir(sub):
                 p = '/'.join([sub,img])
-
                 img = imread(p,as_gray=True)
-                img_r = resize(img,(28,28))
+                img_r = resize(img,(DIM,DIM))
                 x.append(img_r.flatten()) # flatten for other clfs
                 y.append(LETTER_MAP[dir_name])
-
+                counter += 1
+                if counter >= 1000:
+                    break
     x = np.array(x)
     y = np.array(y)
 
@@ -50,100 +79,69 @@ def load_NIST(path):
 
     return X_train,y_train,X_test,y_test
 
+if __name__ == '__main__':
+    # Load OCR Data
+    x_train_NIST,y_train_NIST,x_test_NIST,y_test_NIST = load_NIST(DATA)
 
-# Load OCR Data
-x_train_NIST,y_train_NIST,x_test_NIST,y_test_NIST = load_NIST(DATA)
+    # Decision Tree Train
+    dt_clf = DecisionTreeClassifier()
+    results = cross_val_score(dt_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
+    dt_train_accu = results.mean()
+    print('Decision Tree Training Accuracy: %.2f'%(dt_train_accu))
 
+    # Random Forest Train
+    rf_clf = RandomForestClassifier()
+    results = cross_val_score(rf_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
+    rf_train_accu = results.mean()
+    print('Random Forest Training Accuracy: %.2f'%(rf_train_accu))
 
-# # Perceptron Train
-# pr_clf = Perceptron()
-# results = cross_val_score(pr_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-# mean_accu = results.mean()
-# print('Perceptron Training Accuracy: %.2f'%(mean_accu))
+    # Neural Network Train
+    nn_clf = MLPClassifier()
+    results = cross_val_score(nn_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
+    nn_train_accu = results.mean()
+    print('Neural Network Training Accuracy: %.2f'%(nn_train_accu))
 
-# # Logistic Regression Train
-# lr_clf = LogisticRegression()
-# results = cross_val_score(lr_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-# mean_accu = results.mean()
-# print('Logistic Regression Training Accuracy: %.2f'%(mean_accu))
-
-# # Gaussian NB Train
-# gnb_clf = GaussianNB()
-# results = cross_val_score(gnb_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-# mean_accu = results.mean()
-# print('Gaussian NB Training Accuracy: %.2f'%(mean_accu))
-
-# Decision Tree Train
-dt_clf = DecisionTreeClassifier()
-results = cross_val_score(dt_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-mean_accu = results.mean()
-print('Decision Tree Training Accuracy: %.2f'%(mean_accu))
-
-# Random Forest Train
-rf_clf = RandomForestClassifier()
-results = cross_val_score(rf_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-mean_accu = results.mean()
-print('Random Forest Training Accuracy: %.2f'%(mean_accu))
-
-# Neural Network Train
-nn_clf = MLPClassifier()
-results = cross_val_score(nn_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-mean_accu = results.mean()
-print('Neural Network Training Accuracy: %.2f'%(mean_accu))
-
-# Ensemble Train
-estimators = [('dt',dt_clf),('rf',rf_clf),('nn',nn_clf)]
-en_clf = VotingClassifier(estimators,voting='hard')
-results = cross_val_score(en_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
-mean_accu = results.mean()
-print('Ensemble Training Accuracy: %.2f'%(mean_accu))
+    # Ensemble Train
+    estimators = [('dt',dt_clf),('rf',rf_clf),('nn',nn_clf)]
+    en_clf = VotingClassifier(estimators,voting='hard')
+    results = cross_val_score(en_clf,x_train_NIST,y_train_NIST,cv=5,scoring=('accuracy'))
+    en_train_accu = results.mean()
+    print('Ensemble Training Accuracy: %.2f'%(en_train_accu))
 
 
+    LABELS = ['%d' % i for i in range(28)]
 
 
+    # Decision Tree Test
+    dt_clf.fit(x_train_NIST,y_train_NIST)
+    y_pred = dt_clf.predict(x_test_NIST)
+    dt_test_accu = accuracy_score(y_test_NIST, y_pred)
+    print("Decsision Tree Test Accuracy:",dt_test_accu)
+    print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
 
+    # Random Forest Test
+    rf_clf.fit(x_train_NIST,y_train_NIST)
+    y_pred = rf_clf.predict(x_test_NIST)
+    rf_test_accu = accuracy_score(y_test_NIST, y_pred)
+    print("Random Forest Test Accuracy:",rf_test_accu )
+    print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
 
+    # Neural Network Test
+    nn_clf.fit(x_train_NIST,y_train_NIST)
+    y_pred = nn_clf.predict(x_test_NIST)
+    nn_test_accu = accuracy_score(y_test_NIST, y_pred)
+    print("Neural Network Test Accuracy:",nn_test_accu)
+    print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
 
-LABELS = ['%d' % i for i in range(28)]
+    # Ensemble Test
+    en_clf.fit(x_train_NIST,y_train_NIST)
+    y_pred = en_clf.predict(x_test_NIST)
+    en_test_accu = accuracy_score(y_test_NIST, y_pred)
+    print("Ensemble Test Accuracy:",en_test_accu)
+    print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
 
-# # Perceptron Test
-# pr_clf.fit(x_train_NIST,y_train_NIST)
-# y_pred = pr_clf.predict(x_test_NIST)
-# print("Perceptron Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-# print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
+    train_results = [('DT',dt_train_accu),('RF',rf_train_accu),('NN',nn_train_accu),('EN',en_train_accu)]
 
-# # Logistic Regression Test
-# lr_clf.fit(x_train_NIST,y_train_NIST)
-# y_pred = lr_clf.predict(x_test_NIST)
-# print("Logistic Regression Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-# print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
+    test_results = [('DT',dt_test_accu),('RF',rf_test_accu),('NN',nn_test_accu),('EN',en_test_accu)]
 
-# # Gaussian NB Test
-# gnb_clf.fit(x_train_NIST,y_train_NIST)
-# y_pred = gnb_clf.predict(x_test_NIST)
-# print("Gaussian NB Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-# print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
-
-# Decision Tree Test
-dt_clf.fit(x_train_NIST,y_train_NIST)
-y_pred = dt_clf.predict(x_test_NIST)
-print("Decsision Tree Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
-
-# Random Forest Test
-rf_clf.fit(x_train_NIST,y_train_NIST)
-y_pred = rf_clf.predict(x_test_NIST)
-print("Random Forest Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
-
-# Neural Network Test
-nn_clf.fit(x_train_NIST,y_train_NIST)
-y_pred = nn_clf.predict(x_test_NIST)
-print("Neural Network Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
-
-# Ensemble Test
-en_clf.fit(x_train_NIST,y_train_NIST)
-y_pred = en_clf.predict(x_test_NIST)
-print("Ensemble Test Accuracy: ", accuracy_score(y_test_NIST, y_pred))
-print(classification_report(y_test_NIST, y_pred, target_names=LABELS, digits=3))
+    plot_results(train_results,test_results)
